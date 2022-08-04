57f401ee4ef4aaddb308d6deab3c2eb8953ca03c
Shaddowzinn <84105617+Rain-Design@users.noreply.github.com>
2022-08-03 23:22:45 -0300

    Create Documentation.lua

---
+++ Documentation.lua
@@ -0,0 +1,78 @@
+--[[
+Library:Window({
+    Text = string
+})
+
+Library:Notification({
+    Text = string
+    Duration = number
+    Color = color3
+})
+
+Window:Label({
+    Text = string
+    Color = color3
+})
+
+Label:Set({
+    Text = string
+    Color = color3
+})
+
+Window:Button({
+    Text = string
+    Callback = function
+})
+
+Window:Toggle({
+    Text = string
+    Flag = string
+    Default = boolean
+    Callback = function
+})
+
+Toggle:Set({
+    Bool = boolean
+})
+
+Window:Check({
+    Text = string
+    Flag = string
+    Default = boolean
+    Callback = function
+})
+
+Check:Set({
+    Bool = boolean
+})
+
+Window:Keybind({
+    Text = string
+    Default = enum keycode
+    Callback = function
+})
+
+Window:Slider({
+    Text = string
+    Flag = string
+    Postfix = string (Postfix is what appears after the value of the slider)
+    Default = number
+    Minimum = number
+    Maximum = number
+    Callback = function
+})
+
+Slider doesn't have a Set function yet but i'll make one soon
+
+Window:Dropdown({
+    Text = string
+    List = table
+    Callback = function
+})
+
+Window:Prompt({
+    Text = string
+    OnConfirm = function
+    OnCancel = function
+})
+]]
